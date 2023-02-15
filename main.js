c323e613a5847edd77e138ce594c25a7c5251d3c
Timothy Clark <TimDMClark@gmail.com>
2023-02-15 13:13:40 -0500

    My Intro to Variables

---
+++ main.js
@@ -0,0 +1,17 @@
+//Lightning Exercise
+const firstName = 'Tim';
+const lastName = 'Clark';
+let age = 30;
+let likesTravel = true;
+console.log('Hello, my name is ' + firstName + ' ' + lastName + '!')
+if (likesTravel === true); {
+    console.log('I love to travel!');
+};
+
+
+//Practice
+let temperature = 59;
+let conditions = 'partly cloudy';
+let sunriseTime = '7:19AM';
+let sunsetTime = '6:01PM';
+console.log(`It's currently ${conditions} and ${temperature} degrees Fahrenheit. The sun rose this morning at ${sunriseTime} and will set at ${sunsetTime}.`);
